1945e8e75a1828fcc39e5af67d806c97283bdcee
chol0824 <74773561+chol0824@users.noreply.github.com>
2021-03-27 16:32:31 +0900

    Update RegisActivity.java

--- app/src/main/java/com/hansung/android/soundlogin/RegisActivity.java
+++ app/src/main/java/com/hansung/android/soundlogin/RegisActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,15 +14,9 @@ public class RegisActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_device);
+        setContentView(R.layout.activity_soundcheck);
 
-        Button imageButton = (Button) findViewById(R.id.btn_exit);
-        imageButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        Toast.makeText(RegisActivity.this, "소리를 인식합니다.", Toast.LENGTH_SHORT).show();
+
     }
 }
